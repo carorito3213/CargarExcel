c3b9fc240540127695bbb477f1dbc32a9350192d
Alejandra Hernandez <achc930505@gmail.com>
2024-05-16 09:25:36 -0500

    Se añade el proyecto Java

---
+++ src/test/java/com/CargarExcel/CargarExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CargarExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargarExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
